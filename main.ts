58a2f4cf89293d7978d14d9ac9880289eac4dbd6
ILIKEGT <115674705+ILIKEGT@users.noreply.github.com>
2022-10-13 09:45:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let value = 0
 input.onButtonPressed(Button.A, function () {
+    music.setVolume(127)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 9999, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     value = 1
     while (value == 1) {
         basic.showLeds(`
@@ -45,73 +47,74 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             `)
         basic.pause(1000)
+        music.stopMelody(MelodyStopOptions.Background)
     }
 })
 input.onButtonPressed(Button.B, function () {
     value = 2
     while (value == 2) {
         basic.showLeds(`
-            # . . . .
+            # . # . #
             . . . . .
+            # . . . #
             . . . . .
-            . . . . .
-            . . . . .
+            # . # . #
             `)
         basic.showLeds(`
-            . # . . .
-            # . . . .
+            . # . # .
+            # . # . #
             . . . . .
-            . . . . .
-            . . . . .
+            # . # . #
+            . # . # .
             `)
         basic.showLeds(`
             . . # . .
-            . # . . .
-            # . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . # .
-            . . # . .
-            . # . . .
-            # . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            . . # . .
-            . # . . .
-            # . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . #
-            . . . # .
-            . . # . .
-            . # . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . #
-            . . . # .
+            . # . # .
+            # . # . #
+            . # . # .
             . . # . .
             `)
         basic.showLeds(`
             . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
             . . . . .
-            . . . . .
-            . . . . #
-            . . . # .
             `)
         basic.showLeds(`
             . . . . .
             . . . . .
+            . . # . .
             . . . . .
             . . . . .
-            . . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . # . #
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . # . #
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # # # .
+            # . # . #
+            . # . # .
             `)
         basic.showLeds(`
             . . . . .
